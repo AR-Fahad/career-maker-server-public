da25d28804773f21b7f83ac78c92e6cb7c9850c6
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-11-08 20:48:39 +0600

    services update one method work done

--- controller/services.js
+++ controller/services.js
@@ -1,8 +1,13 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../db/db");
 
 const getServices = async (req, res) => {
+  let query = {};
+  if (req?.query?.email) {
+    query = { provider_email: req.query.email };
+  }
   const servicesCollection = await getDb().collection("services");
-  const result = await servicesCollection.find().toArray();
+  const result = await servicesCollection.find(query).toArray();
   res.send(result);
 };
 
@@ -13,4 +18,22 @@ const setService = async (req, res) => {
   res.send(result);
 };
 
-module.exports = { getServices, setService };
+const updateService = async (req, res) => {
+  const updatedService = req.body;
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const updatingService = {
+    $set: {
+      service_name: updatedService.service_name,
+      img: updatedService.img,
+      area: updatedService.area,
+      description: updatedService.description,
+      price: updatedService.price,
+    },
+  };
+  const servicesCollection = await getDb().collection("services");
+  const result = await servicesCollection.updateOne(query, updatingService);
+  res.send(result);
+};
+
+module.exports = { getServices, setService, updateService };
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { getServices, setService } = require("../controller/services");
+const {
+  getServices,
+  setService,
+  updateService,
+} = require("../controller/services");
 
 const router = express.Router();
 
 // services CRUD
 router.get("/services", getServices);
 router.post("/services", setService);
+router.patch("/services/:id", updateService);
 
 module.exports = { express, router };
