dbbf95e88b6d8507b975419dbe841ae7ab224e92
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-11-09 18:45:46 +0600

    new origin added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://rococo-cocada-6bff95.netlify.app"],
     credentials: true,
   })
 );
